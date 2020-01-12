2cb9bbd1f11d1a58dee495969f5a34840413c515
ArRosid <ahmadrosid30121997@gmail.com>
2020-01-12 17:34:12 +0700

    PollList & PollDetail using Generics Views

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -1,18 +1,27 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from . import models
-from . import serializers
+from . import models, serializers
 
-class PollList(APIView):
-    def get(self, request):
-        pools = models.Poll.objects.all()[:20]
-        data = serializers.PoolSerializer(pools, many=True).data
-        return Response(data)
 
-class PollDetail(APIView):
-    def get(self, request, pk):
-        poll = get_object_or_404(models.Poll, pk=pk)
-        data = serializers.PoolSerializer(poll).data
-        return Response(data)
+# class PollList(APIView):
+#     def get(self, request):
+#         pools = models.Poll.objects.all()[:20]
+#         data = serializers.PoolSerializer(pools, many=True).data
+#         return Response(data)
+
+# class PollDetail(APIView):
+#     def get(self, request, pk):
+#         poll = get_object_or_404(models.Poll, pk=pk)
+#         data = serializers.PoolSerializer(poll).data
+#         return Response(data)
+
+class PollList(generics.ListCreateAPIView):
+    queryset = models.Poll.objects.all()
+    serializer_class = serializers.PoolSerializer
+
+class PollDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Poll.objects.all()
+    serializer_class = serializers.PoolSerializer
