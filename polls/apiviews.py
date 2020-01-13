1c3377d5fb5ad4959be8e3661b16a6e64df82dd2
ArRosid <ahmadrosid30121997@gmail.com>
2020-01-13 12:54:14 +0700

    Set permission for PollsAPI & ChoiceAPI

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate
 from rest_framework import generics, status, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.exceptions import PermissionDenied
 
 from . import models, serializers
 
@@ -31,6 +32,12 @@ class PollViewSet(viewsets.ModelViewSet):
     queryset = models.Poll.objects.all()
     serializer_class = serializers.PoolSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        poll = models.Poll.objects.get(pk=self.kwargs["pk"])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not delete this poll!")
+        return super().destroy(request, *args, **kwargs)
+
 # class ChoiceList(generics.ListCreateAPIView):
 #     queryset = models.Choice.objects.all()
 #     serializer_class = serializers.ChoiceSerializer
@@ -43,6 +50,12 @@ class ChoiceList(generics.ListCreateAPIView):
         queryset = models.Choice.objects.filter(poll_id=self.kwargs["pk"])
         return queryset
     serializer_class = serializers.ChoiceSerializer
+
+    def post(self, request, *args, **kwargs):
+        poll = models.Poll.objects.get(pk=self.kwargs["pk"])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not create choice for this poll!")
+        return super().post(request, *args, **kwargs)
 
 class CreateVote(APIView):
     serializer_class = serializers.VoteSerializer
