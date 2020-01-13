8abc35658cf9cac360cca71d6275bc1e5719fc65
ArRosid <ahmadrosid30121997@gmail.com>
2020-01-13 18:20:21 +0700

    Change Authentication API

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from rest_framework.documentation import include_docs_urls
@@ -17,8 +17,10 @@ urlpatterns = [
     path("polls/<int:poll_pk>/choices/<int:choice_pk>/vote/", apiviews.CreateVote.as_view(), name="create_vote"),
     path("users/", apiviews.UserCreate.as_view(), name="user_create"),
     # path("login/", apiviews.LoginView.as_view(), name="login"),
-    path("login/", views.obtain_auth_token, name="login"),
+    # path("login/", views.obtain_auth_token, name="login"),
     path('docs/', include_docs_urls(title="Polls API", permission_classes=())),
+    path('api-auth/',
+        include("rest_auth.urls")),
 ] 
 
 urlpatterns += router.urls
