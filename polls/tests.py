712d7c57904e73f14f94eb174d0cf2b761911b87
ArRosid <ahmadrosid30121997@gmail.com>
2020-01-13 14:21:15 +0700

    Test Choices List using APIClient

--- polls/tests.py
+++ polls/tests.py
@@ -1,14 +1,15 @@
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
-from rest_framework.test import APIRequestFactory, APITestCase
+from rest_framework.test import APIRequestFactory, APITestCase, APIClient
 
 from polls import apiviews
 
 
 class TestPoll(APITestCase):
     def setUp(self):
-        self.factory = APIRequestFactory()
-        self.view = apiviews.PollViewSet.as_view({"get":"list"})
+        self.client = APIClient()
+        # self.factory = APIRequestFactory()
+        # self.view = apiviews.PollViewSet.as_view({"get":"list"})
         self.uri = "/polls/"
         self.user = self.setup_user()
         self.token = Token.objects.create(user=self.user)
@@ -23,10 +24,16 @@ class TestPoll(APITestCase):
             password='test'
         )
 
+    # def test_list(self):
+    #     request = self.factory.get(self.uri,
+    #         HTTP_AUTHORIZATION=f'Token {self.token.key}')
+    #     request.user = self.user
+    #     response = self.view(request)
+    #     self.assertEqual(response.status_code, 200,
+    #                     f'Expected Response Code 200, received {response.status_code} instead.')
+
     def test_list(self):
-        request = self.factory.get(self.uri,
-            HTTP_AUTHORIZATION=f'Token {self.token.key}')
-        request.user = self.user
-        response = self.view(request)
+        self.client.login(username="test", password="test")
+        response = self.client.get(self.uri)
         self.assertEqual(response.status_code, 200,
                         f'Expected Response Code 200, received {response.status_code} instead.')
